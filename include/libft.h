6d7217be2caad59ac1d60d16e031d4b1b0e9e2e8
AndreIglesias <ciglesia@student.42.fr>
2020-11-18 00:59:47 +0100

    lst_fncts

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: ciglesia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 16:28:56 by ciglesia          #+#    #+#             */
-/*   Updated: 2020/11/13 14:57:44 by ciglesia         ###   ########.fr       */
+/*   Updated: 2020/11/18 00:07:24 by ciglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -156,8 +156,11 @@ t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstrev(t_list **list);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+int					ft_lstlen(t_list *list);
+t_list				*ft_lstidx(t_list *head, int idx);
 
 int					ft_atoi_protected(const char *str);
 int					ft_do_op(int ac, char **av);
